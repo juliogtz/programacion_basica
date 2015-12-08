6e50e4119aec2d085da04674d6f5cc827f41d263
Julio <Julio@iMac-de-Julio.local>
2015-12-08 17:12:24 -0600

    Menejo de variables

--- codigo.js
+++ codigo.js
@@ -1,7 +1,18 @@
 // alert: es una funcion
-	// () dentro de los parentesis van los parametros de la función.
-	alert("Hola mama ya se programar");
-	alert(555*4);
-	alert("555*4");
-	alert("10" + 1);
-	alert(1 + "10");
+// () dentro de los parentesis van los parametros de la función.
+/*alert("Hola mama ya se programar");
+alert(555*4);
+alert("555*4");
+alert("10" + 1);
+alert(1 + "10");*/
+
+//Variables y Funciones
+var nombre ="Julio";
+var apellido = "Gutierrez";
+var edad = 28;
+
+alert(nombre +" "+apellido+" "+edad);
+
+
+
+
