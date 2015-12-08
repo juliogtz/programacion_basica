89afe661214617c125e012e60a2cdbd28363f784
Julio <Julio@iMac-de-Julio.local>
2015-12-08 12:05:02 -0600

    Separar Javascript a otro archivo

---
+++ codigo.js
@@ -0,0 +1,7 @@
+// alert: es una funcion
+	// () dentro de los parentesis van los parametros de la función.
+	alert("Hola mama ya se programar");
+	alert(555*4);
+	alert("555*4");
+	alert("10" + 1);
+	alert(1 + "10");
