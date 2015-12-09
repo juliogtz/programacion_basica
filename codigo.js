f0e7edc43e024976bd271d7402204d57eb272a6a
Julio <Julio@iMac-de-Julio.local>
2015-12-08 18:42:26 -0600

    Peso en Marte

--- codigo.js
+++ codigo.js
@@ -11,8 +11,17 @@ var nombre ="Julio";
 var apellido = "Gutierrez";
 var edad = 28;
 
-alert(nombre +" "+apellido+" "+edad);
+alert(nombre +" "+apellido+"\n"+edad);
 
+
+
+// Algoritmo
+var pesoEnMarte;
+alert("¿Quieres saber tu peso en marte");
+var peso = prompt("¿Por que eres tan gorda?, ¿Cual es tu peso?", 70);
+pesoenMarte = (peso / 9.81) * 3.711;
+alert("Tu peso ideal: "+(parseInt(peso)-10));
+alert("Tu peso en Marte: "+pesoenMarte);
 
 
 
